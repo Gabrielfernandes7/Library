0865dbcd953281900658aed521db53b7cc6ee9cb
Gabrielfernandes7 <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2023-08-21 00:45:08 -0300

    fix: problems about migrate and templates feat: add table rent and modify README.md and fix

--- app/models/models.py
+++ app/models/models.py
@@ -15,7 +15,7 @@ class User(db.Model):
 
 class Book(db.Model):
     __tablename__ = "book"
-    book_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(1000))
 
     def __init__(self, title):
@@ -23,3 +23,22 @@ class Book(db.Model):
     
     def __repr__(self):
         return f"Title Book: {self.title}"
+
+class Rent(db.Model):
+    __tablename__ = "rent"
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_rent = db.Column(db.Date)
+
+    # Object (join)
+    user = db.relationship('User', foreign_keys=user_id)
+    book = db.relationship('Book', foreign_keys=book_id)
+
+    def __init__(self, user_id, book_id, date_rent):
+        self.user_id = user_id
+        self.book_id = book_id
+        self.date_rent = date_rent
+
+    def __repr__(self):
+        return f"Rent {self.id} - {self.user.name} - {self.book.title}"
